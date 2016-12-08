4e4744fd79bdcf394f26bfab458765a9dec71bd6
gtzhong <845272922@qq.com>
2016-12-08 09:42:00 +0800

    1、修改百度编辑器上传图片的存储目录 2、修改商品列表页的图片位置 3、除去下划线

--- assets/ueditor/php/config.php
+++ assets/ueditor/php/config.php
@@ -4,7 +4,8 @@ return array(
 
     //图片上传允许的存储目录
     'imageSavePath' => array (
-        'upload1', 'upload2', 'upload3'
+//        'upload1', 'upload2', 'upload3'
+          '../../admin/editor/upload', '../../admin/editor/upload1', '../../admin/editor/upload2'
     )
 
 );
