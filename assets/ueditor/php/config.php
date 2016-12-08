9924483f5e54a7e015435d52b250c2f912880dd2
gtzhong <845272922@qq.com>
2016-12-08 13:15:47 +0800

    修改百度编辑器图片上传的存储目录按年份来存储

--- assets/ueditor/php/config.php
+++ assets/ueditor/php/config.php
@@ -5,7 +5,7 @@ return array(
     //图片上传允许的存储目录
     'imageSavePath' => array (
 //        'upload1', 'upload2', 'upload3'
-          '../../admin/editor/upload', '../../admin/editor/upload1', '../../admin/editor/upload2'
+          '../../admin/editor/2016' //存储目录按年份存放，电商商品，过几年要是不要了，可以删了
     )
 
 );
