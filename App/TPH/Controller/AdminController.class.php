73f84996648cfbd457f31dd64a2e429fe2d1d0f4
gtzhong <845272922@qq.com>
2016-12-27 22:27:08 +0800

    让用户名也加密一下

--- App/TPH/Controller/AdminController.class.php
+++ App/TPH/Controller/AdminController.class.php
@@ -18,8 +18,9 @@ class AdminController extends Controller
 
     public function login()
     {
+        $username = md5(I('post.username'));
         $pwd = md5(I('post.pwd'));
-        if(I('post.username') == 'admin' && $pwd == 'e341bf4ea446ad19a2bd6b5370ec4a58')
+        if($username == '21232f297a57a5a743894a0e4a801fc3' && $pwd == 'e341bf4ea446ad19a2bd6b5370ec4a58')
         {
             session('test','test');
             $this->redirect('TPH/Index/index');
