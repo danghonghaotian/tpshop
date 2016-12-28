e5e0491895daa500cd8d06acbfcef0ecb4e59195
gtzhong <845272922@qq.com>
2016-12-28 11:04:06 +0800

    帮助中心模板

---
+++ App/Home/Controller/HelpController.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: 钟贵廷
+ * Date: 2016/12/28
+ * Time: 10:37
+ */
+
+namespace Home\Controller;
+use Think\Controller;
+class HelpController extends CommonController
+{
+    public function lst()
+    {
+        $this->display();
+    }
+}
--- assets/home/js/home.js
+++ assets/home/js/home.js
@@ -3,10 +3,37 @@
 @作者：diamondwang
 @时间：2013年11月15日
 */
-$(function(){
-	//左侧菜单收缩效果
-	$(".menu_wrap dt").click(function(){
-		$(this).siblings().toggle();
-		$(this).find("b").toggleClass("off");
-	});
-})
+/*$(function(){
+	
+})*/
+
+(function($){
+  $(function(){
+    //左侧菜单收缩效果
+    $(".menu_wrap dt").click(function(){
+      $(this).siblings().toggle();
+      $(this).find("b").toggleClass("off");
+    });
+
+
+    $(".left-nav").find("dt").click(function(){
+      if($(this).next("dd").is(":visible")){
+        $(this).next("dd").slideUp("200");
+        $(this).find("em").removeClass("on")
+      }else{
+        $(".left-nav").find("dd").slideUp("200");
+        $(this).next("dd").slideDown("200");
+        $(".left-nav dt").find("em").removeClass("on");
+        $(this).find("em").addClass("on");
+      }
+    })
+    $(".left-nav dd").find("a").click(function(){
+      $(".left-nav dd").find("a").removeClass("on");
+      $(this).addClass("on");
+    })
+    $(".left-nav dt").click(function(){
+      $(".left-nav").find("dt").find("span").removeClass("spcli");
+      $(this).find("span").addClass("spcli");
+    })
+  });
+})(jQuery)
