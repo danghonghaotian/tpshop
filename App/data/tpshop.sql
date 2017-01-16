8eeebc60c68bb5716feffc6625aa85054a62e13c
gtzhong <845272922@qq.com>
2017-01-16 09:11:25 +0800

    给货品表添加组合主键

--- App/data/tpshop.sql
+++ App/data/tpshop.sql
@@ -181,7 +181,9 @@ CREATE TABLE `tp_product` (
   `goods_id` mediumint(8) unsigned NOT NULL COMMENT '商品',
   `goods_number` int(10) unsigned NOT NULL COMMENT '库存量',
   `goodsattr_id` varchar(30) NOT NULL COMMENT '属性的ID，如果有多个属性，那么先根据ID升序排序然后再用，号隔开,关联的是sh_goods_attr表的ID',
+  PRIMARY KEY (`goods_id`,`goodsattr_id`),
   KEY `goods_id` (`goods_id`),
   KEY `goodsattr_id` (`goodsattr_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='货品表';
+
 
